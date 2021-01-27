36673a60e668b50dd8ee0563bfd780a845f2475d
ioanaalbu16 <73302014+ioanaalbu16@users.noreply.github.com>
2021-01-27 15:42:50 +0200

    Delete simple_image_write.m

--- simple_image_write.m
+++
@@ -1,8 +0,0 @@
-function simple_image_write(filename, vol, voxdims)
-    fileID = fopen(filename, 'w');
-    fmeta = uint8([size(vol), size(voxdims)]);
-    fwrite(fileID, fmeta, 'uint8');
-    fwrite(fileID, vol, 'int16');
-    fwrite(fileID, voxdims, 'float');
-    fclose(fileID);
-end
