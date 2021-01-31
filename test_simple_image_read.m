d65d45a3a2a8a03e7c5dd1fd14d5c53e4d8cab11
17052580 <73302014+ioanaalbu16@users.noreply.github.com>
2021-01-31 22:38:41 +0100

    Revert "Add files via upload"

---
+++ test_simple_image_read.m
@@ -0,0 +1,17 @@
+% Open the required file with assigned 'read' permission:
+    fileID = fopen('data/example_image.mat', 'r');
+
+% Read the first 5 values (header information indicating size of the image) 
+% into an array (5 elements, single row) with the precision that the source 
+% data class is 'uint8':
+    meta = fread(fileID, [1, 5], 'uint8'); 
+
+% Read the intensity values of the respective image 
+    vol = int16(fread(fileID, prod(meta(1, 1:3)), 'int16'));
+
+% Reshape the intensity values into a matrix
+    vol = reshape(vol, meta(1, 1:3));
+    
+    voxdims = fread(fileID, prod(meta(1, 4:5)), 'float');
+    
+    voxdims = reshape(voxdims, meta(1, 4:5));
